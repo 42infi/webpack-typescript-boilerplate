49329824aa1676c771e71b417c08cf06f2f5172f
42infi <32489011+42infi@users.noreply.github.com>
2024-01-12 13:46:50 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const appDirectory = fs.realpathSync(process.cwd());
+
+module.exports = {
+    entry: path.resolve(appDirectory, "src/index.ts"),
+    output: {
+        filename: "js/app.js",
+        path: path.resolve("./dist/"),
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: {
+        host: "0.0.0.0",
+        port: 8080,
+        static: path.resolve(appDirectory, "public"),
+        hot: true,
+        devMiddleware: {
+            publicPath: "/",
+        },
+        allowedHosts: "all"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve(appDirectory, "public/index.html"),
+        }),
+    ],
+    mode: "development",
+};
